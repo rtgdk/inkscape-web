86b42026179b3bf91c960e6295f8d2d5bb2afe64
Chris Morgan <chris.morganiser@gmail.com>
2011-07-13 18:36:09 +0530

    Put in a general /.../index.php redirect to throw away the index.php bit.

--- djink/apps/content/urls.py
+++ djink/apps/content/urls.py
@@ -28,6 +28,7 @@ urlpatterns += patterns('django.views.generic.simple',
 
     redirect(r'^favicon.ico$', '/media/favicon.ico'),
     redirect(r'^showcase/web_design/(?:index.php)?$', '/showcase/web-design/'),
+    redirect(r'^(?P<dir>.*/)index.php$', '/%(dir)s'),
 )
 
 urlpatterns += patterns('',

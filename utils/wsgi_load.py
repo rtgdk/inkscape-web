2e63456f9bbf6597028f91c52294df5a44d276be
Martin Owens <doctormo@gmail.com>
2013-05-17 12:26:44 -0400

    commit back in some changes made to wsgi

--- utils/wsgi_load.py
+++ utils/wsgi_load.py
@@ -5,14 +5,15 @@ import sys
 
 CODE_PATH = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PATH = os.path.abspath(os.path.join(CODE_PATH, ".."))
+#PROJECT_PATH = '/var/www/staging.inkscape.org' <- Why is this required?
+ACTIVATE = os.path.join(PROJECT_PATH, 'pythonenv/bin/activate_this.py')
 
 sys.path.insert(0, PROJECT_PATH)
 
-print PROJECT_PATH
+execfile( ACTIVATE, dict(__file__=ACTIVATE) )
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'inkscape.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-
 

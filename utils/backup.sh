9acff049cefb6b7bc6a61a24557aff1ed4080baa
Martin Owens <doctormo@gmail.com>
2013-06-01 14:11:01 -0400

    Improved backup with media

--- utils/backup.sh
+++ utils/backup.sh
@@ -4,21 +4,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 BIN="$DIR/pythonenv/bin"
 PYTHON="$BIN/python"
 MANAGE="$DIR/inkscape/manage.py"
-DEST="$DIR/data/media/content"
+DEST="$DIR/data/backup"
 
-mkdir -p $DEST
+mkdir -p $DEST/cms_db
+mkdir -p $DEST/media
 
 DATE=day-`date --iso`.json.gz
 
+# Make a backup of the cms database and put it into a gz json file
+# Don't make a copy of any other databases or tables from django.
 $PYTHON $MANAGE dumpdata cms text menus mptt sekizai file picture \
                 snippet video twitter cmsplugin_news \
-    | gzip -9 > $DEST/$DATED
+    | gzip -9 > $DEST/cms_db/$DATED
 
-cd $DEST
+cd $DEST/cms_db
 
 # Create hard links of all the backups
 ln -f $DATE "latest.json.gz"
-ln -f $DATE week-`date +%W`.json.gz
+ln -f $DATE week-`date +%Y-%W`.json.gz
 ln -f $DATE month-`date +%Y-%M`.json.gz
 ln -f $DATE year-`date +%Y`.json.gz
 
@@ -32,4 +35,11 @@ rm `find . -name "week-*.json.gz" -mtime +42`
 rm `find . -name "month-*.json.gz" -mtime +256`
 
 # Never remove year backups
+# rm `find . -name "year-*.json.gz" -mtime +EEEE`
 
+cd $DIR/data
+
+# Make a backup of all the uploaded media here, archived once per week
+cp -al media $DEST/media/`date +%Y-%W`
+
+# There is room here for cleaning up old media uploads similar to the cms_db

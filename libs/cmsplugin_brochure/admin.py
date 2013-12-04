ef0d27693993e92fc7d66e7037f94ce7adcbda1f
Martin Owens <doctormo@gmail.com>
2013-12-04 08:01:44 -0500

    Enable Admin RSS feed update

--- libs/cmsplugin_brochure/admin.py
+++ libs/cmsplugin_brochure/admin.py
@@ -2,5 +2,17 @@
 from django.contrib import admin
 from cmsplugin_brochure.models import Brochure
 
-admin.site.register(Brochure)
+class BrochureAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publish'
+    list_display = ('name', 'publish')
+    actions = ['_refresh']
+
+    def _refresh(self, request, queryset):
+        for item in queryset:
+            item.refresh()
+
+    _refresh.short_description = 'Refresh Feed Now'
+
+
+admin.site.register(Brochure, BrochureAdmin)
 

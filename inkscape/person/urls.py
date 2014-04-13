1304d35b4b5cc051fcff9bacf1a5ffccd811f880
Martin Owens <doctormo@gmail.com>
2014-04-13 00:53:01 -0400

    Fix registrations urls

--- inkscape/person/urls.py
+++ inkscape/person/urls.py
@@ -1,10 +1,13 @@
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import patterns, url, include
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import patterns, url, include
+
+from django.views.generic.base import TemplateView
 
 from inkscape.person.views import *
-from registration.backends.default.views import ActivationView as AV, RegistrationView as RV
+from registration.backends.default.views import ActivationView as AV, \
+                                                RegistrationView as RV
 
 urlpatterns = patterns('inkscape.person.views',
     url(r'^$',        'my_profile',      name='my_profile'),
@@ -14,10 +17,23 @@ urlpatterns = patterns('inkscape.person.views',
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
-    url(r'^login/',   'login',                name='auth_login'          ),
-    url(r'^logout/',  'logout',               name='auth_logout'         ),
-    url(r'^pwd/$',    'password_change',      name='password_change'     ),
-    url(r'^pwd/y/$',  'password_change_done', name="password_change_done"),
+    url(r'^login/',     'login',                name='auth_login'          ),
+    url(r'^logout/',    'logout',               name='auth_logout'         ),
+    url(r'^pwd/$',      'password_change',      name='password_change'     ),
+    url(r'^pwd/y/$',    'password_change_done', name='password_change_done'),
+)
+
+AC = TemplateView.as_view(template_name='registration/activation_complete.html')
+RC = TemplateView.as_view(template_name='registration/registration_complete.html')
+RK = TemplateView.as_view(template_name='registration/registration_closed.html')
+
+urlpatterns += patterns('',
+    url(r'^activate/complete/$',                AC,           name='registration_activation_complete'),
+    url(r'^activate/(?P<activation_key>\w+)/$', AV.as_view(), name='registration_activate'),
+    url(r'^register/$',                         RV.as_view(), name='registration_register'),
+    url(r'^register/complete/$',                RC,           name='registration_complete'),
+    url(r'^register/closed/$',                  RK,           name='registration_disallowed'),
+    url(r'^register/',                          include('registration.auth_urls')),
 )
 
 

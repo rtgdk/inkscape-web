53bc865ec64b01d23df54f52f072933e6b005609
Martin Owens <doctormo@gmail.com>
2014-04-20 23:21:42 -0400

    Put the libs setting back, local installs son't need it, but live sure does.

--- inkscape/settings.py
+++ inkscape/settings.py
@@ -55,6 +55,8 @@ except ImportError:
   except ImportError:
       logging.error("No settings found and default template failed to load.")
 
+sys.path.insert(0, os.path.join(PROJECT_PATH, 'libs'))
+
 (VERSION_STRING, INKSCAPE_VERSION) = get_bzr_version(PROJECT_PATH, DEBUG)
 
 LOCALE_PATHS = os.path.join(PROJECT_PATH, 'locale'),

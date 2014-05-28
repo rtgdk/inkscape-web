feb89d4dedc30afeff0a457813ed78916c3f355b
Martin Owens <doctormo@gmail.com>
2014-05-28 17:49:50 -0400

    Move gallery drag drop to generic location.

--- static/js/inkscape.js
+++ static/js/inkscape.js
@@ -5,3 +5,65 @@ function getPng(t) {
   }
 }
 
+/* Resize menu bar code (for translations) */
+$(document).ready(function() {
+      if($("#menu"))menu();
+      if($('[class|="maxHeight"]')[0])maxHeight();
+      if($("#toplogin"))adjustBar();
+});
+
+jQuery.fn.getMaxHeight = function(){
+    var ca = this.attr('class');
+    var rval = [];
+    if(ca && ca.length && ca.split){
+        ca = jQuery.trim(ca); /* strip leading and trailing spaces */
+        ca = ca.replace(/\s+/g,' '); /* remove doube spaces */
+        var n = ca.indexOf("maxHeight-"); 
+        ca.substring(n);
+        rval = ca.split(' ');
+    }
+    return rval[0].replace("maxHeight-","");
+}
+
+function menu(){
+    var elementHeight=0;
+    var containerHeight=1;
+    var i = 0;
+    var onePixelLess = parseInt( $("#menu").children("li").children("a:first-child").css('font-size'))-1 + "px";
+    while(containerHeight > elementHeight){
+        $("#menu").children("li").each(function(){
+            if(i != 0){
+                $(this).children("a:first-child").css('font-size',onePixelLess);
+                $(this).children("a:first-child").css('padding-left',parseInt($(this).children("a:first-child").css('padding-left'))-1 + "px");
+                $(this).children("a:first-child").css('padding-right',parseInt($(this).children("a:first-child").css('padding-right'))-1 + "px");
+            }
+            elementHeight = $(this).height();
+        })
+        i++;
+        containerHeight = $("#menu").height();
+    }
+}
+
+function adjustBar(){
+    var tw = parseInt( $("#toplogin").width() );
+    var fw = parseInt( $("#toplogin").children("form").width() );
+    $("#toplogin").children("form").css('margin-left', (tw-fw-20)+"px");
+}
+
+function maxHeight(){
+    $('[class|="maxHeight"]').each(function(){
+        var elementHeight = $(this).height();
+        var containerHeight=$(this).getMaxHeight();
+        var textSize;
+        var i = 0;
+        while(containerHeight < elementHeight){
+            textSize =  parseInt($(this).css('font-size'));
+            if(textSize == 5) break;
+           if(i != 0){
+                $(this).css('font-size',textSize-1 + "px");
+            }
+            i++;
+            elementHeight = $(this).height();
+        }
+    })
+}

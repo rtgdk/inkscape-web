2b0ce0373b3dbda3ad7c5cc0a6644e5d588b6901
Martin Owens <doctormo@gmail.com>
2014-09-27 20:51:33 -0400

    Fix issues with how we call the make language directive.

--- utils/make_lang.sh
+++ utils/make_lang.sh
@@ -1,5 +1,9 @@
 
-MANAGE="./utils/manage"
+source pythonenv/bin/activate
+
+MANAGE="./inkscape/manage.py"
+
+mkdir -p locale
 
 $MANAGE makemessages --locale=$1 --ignore=PIL --ignore=html5lib --ignore=compositekey --ignore=easy_thumbnails
 $MANAGE compilemessages --locale=$1

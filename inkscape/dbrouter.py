6a83ea7d6b732e5da5f0a886f66bb6e7498d14e9
Martin Owens <doctormo@gmail.com>
2014-10-03 10:05:41 -0400

    Updated dbrouter for news articles

--- inkscape/dbrouter.py
+++ inkscape/dbrouter.py
@@ -2,18 +2,24 @@
 
 class OldRouter(object):
     def db_for_read(self, model, **hints):
+        if model.__name__ == 'News':
+            return 'old'
         if 'instance' in hints:
             db = hints['instance']._state.db
             if db:
                 return db
-        if model.__name__ == 'News':
-            return 'old'
 
     def db_for_write(self, model, **hints):
+        if model.__name__ == 'News':
+            return 'old'
         if 'instance' in hints:
             db = hints['instance']._state.db
             if db:
                 return db
-        if model.__name__ == 'News':
-            return 'old'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'auth' or \
+           obj2._meta.app_label == 'auth':
+           return True
+        return None
 

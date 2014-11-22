2533fc4d0e80969cbcbf4c12bfe2fc1d998acfae
Martin Owens <doctormo@gmail.com>
2014-11-22 00:26:49 -0500

    Add hack to correct for in_navigation error regarding menus

--- inkscape/extra/templatetags/extras.py
+++ inkscape/extra/templatetags/extras.py
@@ -2,6 +2,8 @@
 from django.template.base import Library
 from django.conf import settings
 
+from cms.models.pagemodel import Page
+
 register = Library()
 
 @register.filter("placeholder")
@@ -11,4 +13,17 @@ def add_placeholder(form, text=None):
     form.field.widget.attrs.update({ "placeholder": text })
     return form
 
+@register.filter("root_nudge")
+def root_nudge(root, page):
+    """
+    django cms has a serious flaw with how it manages the 'root' of menus.
+    This is because cms attempts to manage the root sent to menu via
+    checking in_navigation options. this conflicts with the menus.NavigationNode
+    system which controls the system via the templates.
+
+    This tag stops django-cms interfering by correcting menus input when needed.
+    """
+    page = Page.objects.get(is_home=True, publisher_is_draft=page.publisher_is_draft)
+    return root + int(page.in_navigation)
+    
 

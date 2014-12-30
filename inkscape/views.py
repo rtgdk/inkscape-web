403a34ecd5e445f91cd2d74621022f73d1c4fc36
Martin Owens <doctormo@gmail.com>
2014-12-30 08:04:54 -0500

    Fix live issue with redirects, status code work needs to issue correct status code.

--- inkscape/views.py
+++ inkscape/views.py
@@ -4,8 +4,10 @@ from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
 def error(request, **c):
-    return render_to_response('error/%s.html' % c['error'], c,
+    response = render_to_response('error/%s.html' % c['error'], c,
         context_instance=RequestContext(request))
+    response.status_code = int(c['error'])
+    return response
 
 def error403(request):
     return error(request, error='403', title=_('Permission Denied'))

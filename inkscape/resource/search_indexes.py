d86003b6793aa046d0b30d188efbb8db9903115a
Martin Owens <doctormo@gmail.com>
2015-01-12 00:13:10 -0500

    Fix search indexing for gallery listings.

--- inkscape/resource/search_indexes.py
+++ inkscape/resource/search_indexes.py
@@ -3,9 +3,11 @@ from haystack.indexes import *
 from .models import Resource
 
 class ResourceIndex(SearchIndex, Indexable):
-    text     = CharField(document=True, use_template=True)
-    edited   = DateTimeField(model_attr='edited')
-    category = CharField(model_attr='category')
+    text      = CharField(document=True, use_template=True)
+
+    edited    = DateTimeField(model_attr='edited')
+    created   = DateTimeField(model_attr='created')
+    published = BooleanField(model_attr='published')
 
     def get_model(self):
         return Resource
@@ -16,4 +18,11 @@ class ResourceIndex(SearchIndex, Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(published=True)
+
+from .views import GalleryList
+from pile.search_base import add_fields
+
+# This adds the extra indexable fields that the category list uses.  
+add_fields(ResourceIndex, GalleryList)
+
 

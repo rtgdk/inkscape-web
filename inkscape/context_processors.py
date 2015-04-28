8e327a31d8267be653211f42d4399384f3efa781
Martin Owens <doctormo@gmail.com>
2015-04-28 11:53:46 -0400

    Fixes for recent changes and updated restore protocols

--- inkscape/context_processors.py
+++ inkscape/context_processors.py
@@ -1,4 +1,10 @@
 
+import os
+import sys
+import email
+import django
+import logging
+
 from django.conf import settings
 
 def design(request):
@@ -6,40 +12,31 @@ def design(request):
     Adds static-related context variables to the context.
     """
     return {
-        'DEBUG': settings.DEBUG,
-        'GOOGLE_ANID': settings.GOOGLE_ANID,
+      'DEBUG': settings.DEBUG,
+      'GOOGLE_ANID': settings.GOOGLE_ANID,
     }
-
-import os
-import sys
-import email
-import django
-import logging
-
-from os.path import join, dirname
-from django.conf import settings
 
 INKSCAPE_VERSION = ''
 WEBSITE_VERSION = ''
 WEBSITE_REVISION = ''
 
-VERSION_FILE = os.path.join(settings.PROJECT_DIR, 'version')
+VERSION_FILE = os.path.join(settings.PROJECT_PATH, 'version')
 if os.path.exists(VERSION_FILE):
-    emai_msg = email.message_from_file(open(version_file))
+    emai_msg = email.message_from_file(open(VERSION_FILE))
     WEBSITE_VERSION = emai_msg["version"]
     INKSCAPE_VERSION = emai_msg["inkscape"]
 
-REVISION_FILE = os.path.join(PROJECT_PATH, 'data', 'revision')
+REVISION_FILE = os.path.join(settings.PROJECT_PATH, 'data', 'revision')
 if os.path.isfile(REVISION_FILE):
-    with open(REV_FILE, 'r') as fhl:
+    with open(REVISION_FILE, 'r') as fhl:
         REVISION = fhl.read().strip()
 
 def version(request):
-    return {'INKSCAPE_VERSION': INKSCAPE_VERSION,
-            'WEBSITE_REVISION': WEBSITE_REVISION,
-            'WEBSITE_VERSION': WEBSITE_VERSION,
-            'CONTENT_VERSION': bzr_revision(),
-            'DJANGO_VERSION': django.get_version(),
-            'PYTHON_VERSION': sys.version.split()[0]
-            }
+    return {
+      'INKSCAPE_VERSION': INKSCAPE_VERSION,
+      'WEBSITE_REVISION': WEBSITE_REVISION,
+      'WEBSITE_VERSION': WEBSITE_VERSION,
+      'DJANGO_VERSION': django.get_version(),
+      'PYTHON_VERSION': sys.version.split()[0]
+    }
 

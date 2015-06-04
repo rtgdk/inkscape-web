cbdb052d404fea8a8c4485fc7b50f71326330560
Moini <moini@noreply.invalid>
2015-06-04 23:21:55 +0200

    add migrations for contests start/end date and some name improvements for teams admin

---
+++ resource/migrations/0003_auto_20150604_2027.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('resource', '0002_auto_20150517_1743'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='end_contest',
+            field=models.DateField(null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='start_contest',
+            field=models.DateField(help_text=b'If specified, this category will have special voting rules.', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]

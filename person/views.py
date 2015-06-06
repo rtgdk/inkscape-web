c52ac35c1ebfddfdaff443f59932378820538968
Martin Owens <doctormo@gmail.com>
2015-06-05 22:20:08 -0400

    Move GPG view and friend views to class based views

--- person/views.py
+++ person/views.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import get_object_or_404, render_to_response, redirect
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.template import RequestContext
-
-from django.views.generic import UpdateView, DetailView
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
-
+from django.views.generic import UpdateView, DetailView, ListView, RedirectView
+from django.views.generic.detail import SingleObjectMixin
 from user_sessions.views import LoginRequiredMixin
+from django.contrib import messages
 
-from .models import UserDetails, Team
+from .models import User, UserDetails, Group, Team
 from .forms import PersonForm
-from pile.views import *
 
 class UserMixin(LoginRequiredMixin):
     def get_object(self):
@@ -25,17 +18,9 @@ class EditProfile(UserMixin, UpdateView):
     def get_success_url(self):
         return self.get_object().get_absolute_url()
 
-
-
-from django.contrib.auth.decorators import user_passes_test
-@user_passes_test(lambda u: u.is_superuser)
-def view_profiles(request):
-    c = {
-        'users': UserDetails.objects.all(),
-    }
-    return render_to_response('person/profiles.html', c,
-        context_instance=RequestContext(request))
-
+class FacesView(LoginRequiredMixin, ListView):
+    template_name = 'person/profiles.html'
+    queryset = UserDetails.objects.all()
 
 class UserDetail(DetailView):
     template_name  = 'person/user_detail.html'
@@ -43,36 +28,50 @@ class UserDetail(DetailView):
     slug_field     = 'username'
     model = User
 
-    def get_context_data(self, **kwargs):
-        data = super(UserDetail, self).get_context_data(**kwargs)
-        data['object'].visited_by(self.request.user)
-        return data
+    def get_object(self, **kwargs):
+        user = super(UserDetail, self).get_object(**kwargs)
+        user.visited_by(self.request.user)
+        return user
+
+class UserGPGKey(UserDetail):
+    template_name = 'person/gpgkey.txt'
+    content_type = "text/plain"
 
 class MyProfile(UserMixin, UserDetail):
     pass
 
+# ====== FRIENDSHIP VIEWS =========== #
+
+class MakeFriend(LoginRequiredMixin, SingleObjectMixin, RedirectView):
+    slug_url_kwarg = 'username'
+    slug_field     = 'username'
+    model = User
+
+    def get_object(self, **kwargs):
+        user = super(type(self), self).get_object(**kwargs)
+        (obj, new) = self.request.user.friends.get_or_create(user=user)
+        if new:
+            messages.success(self.request, "Friendship created with %s" % str(user))
+        else:
+            messages.error(self.request, "Already a friend with %s" % str(user))
+        return user
+
+    def get_redirect_url(self):
+        return self.get_object().get_absolute_url()
+
+class LeaveFriend(MakeFriend):
+    def dispatch(self, **kwargs):
+        user = super(type(self), self).get_object(**kwargs)
+        self.request.user.friends.delete(user=user)
+        messages.success(self.request, "Friendship removed from %s" % str(user))
+        return user
+
+
+# ====== TEAM VIEWS ====== #
+
 class TeamDetail(DetailView):
-    model = Team
     slug_url_kwarg = 'team_slug'
-
-@login_required
-def add_friend(request):
-    # XXX to-do
-    to_user = get_object_or_404(User, username=request.GET.get('u', '-1'))
-    friendship = request.user.friends.create(friend=to_user)
-    return HttpResponse(friendship.pk)
-
-@login_required
-def rem_friend(request, friend_id):
-    friendship = get_object_or_404(Friend, pk=friend_id, user=request.user)
-    friendship.delete()
-    return HttpResponse(1)
-
-def gpg_key(request, username):
-    user = get_object_or_404(User, username=username)
-    return render_to_response('person/gpgkey.txt', { 'user': user },
-      context_instance=RequestContext(request),
-      content_type="text/plain")
+    model = Team
 
 
 

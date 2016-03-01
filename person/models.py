b4cb6b3deabc08549ed93f68678e2171126b4c82
Moini <moini@noreply.invalid>
2016-03-01 18:20:37 +0100

    simplify help text string

--- person/models.py
+++ person/models.py
@@ -50,15 +50,14 @@ class User(AbstractUser):
     ocuser  = CharField(_('Openclipart User'), max_length=64, **null)
     tbruser = CharField(_('Tumblr User'), max_length=64, **null)
     gpg_key = TextField(_('GPG Public Key'),
-        help_text=_("".join(
-          ('<strong>Signing and Checksums for Uploads</strong><br/> ',
-          'Either fill in a valid GPG key, so you can sign your uploads, ',
-          'or just enter any text to activate the upload validation feature ',
-          'which verifies your uploads by comparing checksums.<br/>',
-          '<strong>Usage in file upload/editing form:</strong><br/>',
-          'If you have submitted a GPG key, you can upload a *.sig file, ',
-          'and your upload can be verified. You can also submit these checksum file types:<br/>',
-          '*.md5, *.sha1, *.sha224, *.sha256, *.sha384 or *.sha512',))),
+        help_text=_('<strong>Signing and Checksums for Uploads</strong><br/> '
+                    'Either fill in a valid GPG key, so you can sign your uploads, '
+                    'or just enter any text to activate the upload validation feature '
+                    'which verifies your uploads by comparing checksums.<br/>'
+                    '<strong>Usage in file upload/editing form:</strong><br/>'
+                    'If you have submitted a GPG key, you can upload a *.sig file, '
+                    'and your upload can be verified. You can also submit these checksum file types:<br/>'
+                    '*.md5, *.sha1, *.sha224, *.sha256, *.sha384 or *.sha512'),
         validators=[MaxLengthValidator(262144)], **null)
 
     last_seen = DateTimeField(**null)

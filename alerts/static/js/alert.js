bce5b88698e5a9930d44e316debcd5a313a2a711
Sylvain Chiron <chironsylvain@orange.fr>
2016-06-30 20:31:53 +0200

    Fixed style of profile pages' breadcrumbs + little CSS fix for buttons + some JS indentation

--- alerts/static/js/alert.js
+++ alerts/static/js/alert.js
@@ -19,15 +19,14 @@
 //
 
 $(document).ready(function() {
-  $('.runner').submit(function(){
-     jQuery.ajax({
-        url: $(this).attr("action"),
-        type: 'get',
-        success:function(data)
-        {
-            $('#alert_'+data).remove();
-        }, 
-     });
+  $('.runner').submit(function() {
+    jQuery.ajax({
+      url: $(this).attr("action"),
+      type: 'get',
+      success: function(data) {
+        $('#alert_'+data).remove();
+      },
+    });
     return false;
   });
 });
